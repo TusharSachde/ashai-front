38302233a1ad3f3f57132b2d4887fa0384e35c84
sapanab <basutkar.sapana3@gmail.com>
2015-03-12 12:29:05 +0530

    slider added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var firstapp = angular.module('firstapp', [
   'phonecatControllers',
   'templateservicemod',
     'navigationservice'
+  
 ]);
 
 firstapp.config(['$routeProvider',
@@ -102,3 +103,13 @@ firstapp.config(['$routeProvider',
       
     
   }]);
+
+firstapp.directive('wrapOwlcarousel', function () {  
+    return {  
+        restrict: 'E',  
+        link: function (scope, element, attrs) {  
+            var options = scope.$eval($(element).attr('data-options'));  
+            $(element).owlCarousel(options);  
+        }  
+    };  
+});    
