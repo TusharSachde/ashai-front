42ee21c3781516b37923f7a14383f7ed1198c4d6
jagruti <patiljagruti181@gmail.com>
2015-03-24 16:39:22 +0530

    header

--- js/app.js
+++ js/app.js
@@ -160,6 +160,27 @@ firstapp.filter('fourletter', function () {
 
         };
     });
+
+firstapp.filter('cut', function () {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || ' …');
+        };
+    });
+
 firstapp.filter('rawHtml', ['$sce', function($sce){
   return function(val) {
     return $sce.trustAsHtml(val);
