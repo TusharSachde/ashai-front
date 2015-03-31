524080cce2f090d0c7547158b24bef1fc486eb5d
jagruti <patiljagruti181@gmail.com>
2015-03-31 17:44:55 +0530

    last try video

--- js/app.js
+++ js/app.js
@@ -126,6 +126,24 @@ firstapp.directive('wrapOwlcarousel', function () {
     };  
 });  
 
+
+firstapp.directive('myYoutube', function($sce) {
+  return {
+    restrict: 'EA',
+    scope: { code:'=' },
+    replace: true,
+    template: '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+    link: function (scope) {
+        console.log('here');
+        scope.$watch('code', function (newVal) {
+           if (newVal) {
+               scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal);
+           }
+        });
+    }
+  };
+});
+
 firstapp.filter('categoryimagepath', function () {
     return function (input) {
         if (input == null || input == '') {
