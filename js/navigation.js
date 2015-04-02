1e6e7ffe64ed221df77871aba1d7784aca7bfbff
sohan1 <shn619@gmail.com>
2015-04-02 18:22:38 +0530

    profile-im

--- js/navigation.js
+++ js/navigation.js
@@ -1,149 +1,129 @@
 var admin_url = "http://wohlig.co.in/powerforone/index.php/";
- var admin_url = "http://localhost/powerforone/index.php/";
+// var admin_url = "http://localhost/powerforone/index.php/";
 // var adminurl = "http://wohlig.co.in/powerforone/";
 var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function ($http) {
     var navigation = [{
         name: "Explore projects",
-         classis: "",
-        link:"#/explore/0",
+        classis: "",
+        link: "#/explore/0",
         subnav: []
     }, {
         name: "How it works",
         active: "",
-        link:"#/works",
+        link: "#/works",
         subnav: []
     }, {
         name: "The fellowship",
         classis: "",
-        link:"#/fellowship",
+        link: "#/fellowship",
         subnav: []
     }];
-    
+
     var donation = [{
-        val:"500",
-        period:"One Time",
-        select:"selected"
-    },{
-        val:"500",
-        period:"Monthly",
-        select:""
-    },{
-        val:"500",
-        period:"Quarterly",
-        select:""
-    },{
-        val:"500",
-        period:"Yearly",
-        select:""
-    },];
+        val: "500",
+        period: "One Time",
+        select: "selected"
+    }, {
+        val: "500",
+        period: "Monthly",
+        select: ""
+    }, {
+        val: "500",
+        period: "Quarterly",
+        select: ""
+    }, {
+        val: "500",
+        period: "Yearly",
+        select: ""
+    }, ];
 
     return {
-        getnav: function() {
+        getnav: function () {
             return navigation;
         },
-         displayfrmdb:function(callback)
-          {
-              var category = 0;
-              $http.get(admin_url+ 'json/getprojectbycategory?category=' + category,{}).success(callback);
-          },
-         setprojectid:function(id)
-          {
-              $.jStorage.set("projectid",id);
-          },
-         getdonation:function()
-          {
-              return donation;
-          },
-         getprojectid:function(id)
-          {
-              return $.jStorage.get("projectid");
-          },
-         createfrontendorder:function(checkout)
-          {
-              console.log(checkout.name);
-              return $http({
+        displayfrmdb: function (callback) {
+            var category = 0;
+            $http.get(admin_url + 'json/getprojectbycategory?category=' + category, {}).success(callback);
+        },
+        setprojectid: function (id) {
+            $.jStorage.set("projectid", id);
+        },
+        getdonation: function () {
+            return donation;
+        },
+        getprojectid: function (id) {
+            return $.jStorage.get("projectid");
+        },
+        createfrontendorder: function (checkout) {
+            console.log(checkout.name);
+            return $http({
                 url: admin_url + "json/createfrontendorder",
                 method: "POST",
                 data: {
-					"name" : checkout.name,
-					"email" : checkout.email,
-                    "mobile" : checkout.mobile,
-                    "city" : checkout.city,
-                    "address" : checkout.address,
-                    "pan" : checkout.pan,
-                    "dob" : checkout.dob,
-                    "project" : checkout.project,
-                    "amount" : checkout.amount,
-                    "istax" : checkout.istax,
-                    "anonymous" : checkout.anonymous
+                    "name": checkout.name,
+                    "email": checkout.email,
+                    "mobile": checkout.mobile,
+                    "city": checkout.city,
+                    "address": checkout.address,
+                    "pan": checkout.pan,
+                    "dob": checkout.dob,
+                    "project": checkout.project,
+                    "amount": checkout.amount,
+                    "istax": checkout.istax,
+                    "anonymous": checkout.anonymous
                 }
             })
-          },
-         getsingleblog:function(id)
-          {
-              return $http.get(admin_url+ 'json/getsingleblog?id=' + id,{});
-          },
-         viewstaticpage:function(id)
-          {
-              return $http.get(admin_url+ 'json/viewstaticpage?id=' + id,{});
-          },
-          getallcouponold:function()
-          {
-              return $http.get(admin_url+ 'json/getallcoupon',{});
-          },
-          authenticate:function()
-          {
-              return $http.get(admin_url+ 'json/authenticate',{});
-          },
-          getsingleuser :function()
-          {
-              return $http.get(admin_url+ 'json/getsingleuser ',{});
-          },
-         getblog:function()
-          {
-              return $http.get(admin_url+ 'json/getallblog',{});
-          },
-         getstaticpages:function()
-          {
-              return $http.get(admin_url+ 'json/getstaticpages',{});
-          },
-         getprojectbycategoryarray:function(category)
-          {
-              return $http.get(admin_url+ 'json/getprojectbycategoryarray?category=' + category,{});
-          },
-         getallproject:function(category)
-          {
-              return $http.get(admin_url+ 'json/getprojectbycategory?category=' + category,{});
-          },
-         getsinglestaticpage:function(id)
-          {
-              return $http.get(admin_url+ 'json/getsinglestaticpage?id=' + id,{});
-          },
-         displaytestmonial:function(callback)
-          {
+        },
+        getsingleblog: function (id) {
+            return $http.get(admin_url + 'json/getsingleblog?id=' + id, {});
+        },
+        viewstaticpage: function (id) {
+            return $http.get(admin_url + 'json/viewstaticpage?id=' + id, {});
+        },
+        getallcouponold: function () {
+            return $http.get(admin_url + 'json/getallcoupon', {});
+        },
+        authenticate: function () {
+            return $http.get(admin_url + 'json/authenticate', {});
+        },
+        getsingleuser: function () {
+            return $http.get(admin_url + 'json/getsingleuser ', {});
+        },
+        getblog: function () {
+            return $http.get(admin_url + 'json/getallblog', {});
+        },
+        getstaticpages: function () {
+            return $http.get(admin_url + 'json/getstaticpages', {});
+        },
+        getprojectbycategoryarray: function (category) {
+            return $http.get(admin_url + 'json/getprojectbycategoryarray?category=' + category, {});
+        },
+        getallproject: function (category) {
+            return $http.get(admin_url + 'json/getprojectbycategory?category=' + category, {});
+        },
+        getsinglestaticpage: function (id) {
+            return $http.get(admin_url + 'json/getsinglestaticpage?id=' + id, {});
+        },
+        displaytestmonial: function (callback) {
 
-              $http.get(admin_url+ 'json/viewtestimonialjson').success(callback);
-          },
-         getallcategory:function()
-          {
+            $http.get(admin_url + 'json/viewtestimonialjson').success(callback);
+        },
+        getallcategory: function () {
 
-              return $http.get(admin_url+ 'jsonbyavi/getallcategory',{});
-          },
-         logout:function()
-          {
+            return $http.get(admin_url + 'jsonbyavi/getallcategory', {});
+        },
+        logout: function () {
 
-              return $http.get(admin_url+ 'json/logout',{});
-          },
-         getsingleproject:function(id,callback)
-          {
+            return $http.get(admin_url + 'json/logout', {});
+        },
+        getsingleproject: function (id, callback) {
 
-              $http.get(admin_url+ 'json/getsingleproject?id='+id).success(callback);
-          },
-         login:function(user)
-          {
-             return $http({
+            $http.get(admin_url + 'json/getsingleproject?id=' + id).success(callback);
+        },
+        login: function (user) {
+            return $http({
                 url: admin_url + "json/login",
                 method: "POST",
                 data: {
@@ -151,10 +131,9 @@ var navigationservice = angular.module('navigationservice', [])
                     'password': user.password
                 }
             });
-          },
-         register:function(user)
-          {
-             return $http({
+        },
+        register: function (user) {
+            return $http({
                 url: admin_url + "json/register",
                 method: "POST",
                 data: {
@@ -163,34 +142,32 @@ var navigationservice = angular.module('navigationservice', [])
                     'password': user.password
                 }
             });
-          },
-        makeactive: function(menuname) {
-            for(var i=0;i<navigation.length;i++) {
-                if(navigation[i].name==menuname)
-                {
-                    navigation[i].classis="active";
-                }
-                else {
-                    navigation[i].classis="";
+        },
+        makeactive: function (menuname) {
+            for (var i = 0; i < navigation.length; i++) {
+                if (navigation[i].name == menuname) {
+                    navigation[i].classis = "active";
+                } else {
+                    navigation[i].classis = "";
                 }
             }
             return menuname;
         },
- 		submitcontactform: function(form) {
-            return $http.post(admin_url + "json/createcontactus",form);
+        submitcontactform: function (form) {
+            return $http.post(admin_url + "json/createcontactus", form);
         },
-		  submitworkwithus: function(form1) {
-            return $http.post(admin_url + "json/createworkwithus",form1);
+        submitworkwithus: function (form1) {
+            return $http.post(admin_url + "json/createworkwithus", form1);
         },
-		  submitpfo: function(form2) {
-            return $http.post(admin_url + "json/createpfo",form2);
+        submitpfo: function (form2) {
+            return $http.post(admin_url + "json/createpfo", form2);
         },
-		submitnewsletter: function(news) {
-				return $http({
+        submitnewsletter: function (news) {
+            return $http({
                 url: admin_url + "json/createnewsletter",
                 method: "POST",
                 data: {
-					"email" : news.email
+                    "email": news.email
                 }
             })
         }
